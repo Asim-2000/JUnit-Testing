8d407ce9ccba095c934b7f077c29a93d3a5c94fa
Muhammad Asim Khaskheli <m.asimkhaskheli@gmail.com>
2021-04-23 03:14:14 +0500

    refactored ClincCalender to create new method

--- src/patientintake/ClinicCalendar.java
+++ src/patientintake/ClinicCalendar.java
@@ -21,6 +21,12 @@ public class ClinicCalendar {
 
 	public void addAppointment(String patientFirstName, String patientLastName, String doctorKey, String dateTime) {
 		Doctor doc = Doctor.valueOf(doctorKey.toLowerCase());
+		LocalDateTime localDateTime = convertToDateFromString(dateTime);
+		PatientAppointment appointment = new PatientAppointment(patientFirstName, patientLastName, localDateTime, doc);
+		appointments.add(appointment);
+	}
+
+	private LocalDateTime convertToDateFromString(String dateTime) {
 		LocalDateTime localDateTime;
 		try {
 			if (dateTime.toLowerCase().startsWith("today")) {
@@ -36,8 +42,7 @@ public class ClinicCalendar {
 			throw new RuntimeException("Unable to create date time from: [" + dateTime
 					+ "], please enter with format [M/d/yyyy h:mm a], " + t.getMessage());
 		}
-		PatientAppointment appointment = new PatientAppointment(patientFirstName, patientLastName, localDateTime, doc);
-		appointments.add(appointment);
+		return localDateTime;
 	}
 
 	public List<PatientAppointment> getAppointments() {
