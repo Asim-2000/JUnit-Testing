660dbdc51259ee64c5b82cfd5df913ec54ad22af
Muhammad Asim Khaskheli <m.asimkhaskheli@gmail.com>
2021-05-01 22:21:31 +0500

    📦 NEW: Added TestSuite and TestRunner

---
+++ test/appointments/TestSuite.java
@@ -0,0 +1,13 @@
+package appointments;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        ConversionofdateTimeTest.class,
+        DocCalendarShouldTest.class
+})
+
+public class TestSuite {
+}
